c2512946ce8a302d63ba12327b200cc2aff2610e
AvidRP <s2pant@edu.uwaterloo.ca>
2017-08-11 16:18:04 -0600

    another thread for network tasks

--- EatOut/app/src/main/java/com/example/android/eatout/MainActivity.java
+++ EatOut/app/src/main/java/com/example/android/eatout/MainActivity.java
@@ -1,17 +1,53 @@
 package com.example.android.eatout;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+
+    Button searchButton;
+
+    TextView searchResultsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editTextSearchQuery);
+
+        searchButton = (Button) findViewById(R.id.searchButton);
+
+        searchResultsTextView = (TextView) findViewById(R.id.textViewDisplayResults);
+
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeSearchQuery();
+            }
+        });
+    }
+
+    public void makeSearchQuery() {
+        //need make the url, so call buildUrl
+        String searchQuery = editText.getText().toString();
+        URL newSearchUrl = NetworkUtils.buildUrl(searchQuery);
+        Toast.makeText(MainActivity.this, "Searching..", Toast.LENGTH_SHORT).show();
+        new searchTask().execute(newSearchUrl);
     }
 
     //to display the navbar
@@ -38,4 +74,34 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    //have to do all connection stuff in another thread otherwise the app crashes.
+    public class searchTask extends AsyncTask <URL, Void, String> {
+
+        //so get the http response in the background
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchUrl = urls[0];
+            String searchResults = null;
+            try {
+                searchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
+            return searchResults;
+        }
+
+        //after the background task is complete
+        @Override
+        protected void onPostExecute(String s) {
+            //have to see if its still null
+            if(s != null && !s.equals("")) {
+                searchResultsTextView.setText(s);
+            }
+            super.onPostExecute(s);
+        }
+    }
+
+
 }
